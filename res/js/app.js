a3f1f577310479dcc9f14f7095def1e096e890ab
xdang <xdang@paypal.com>
2016-06-23 18:53:19 +0800

    Thu 23 Jun - Finalize GMaps

--- res/js/app.js
+++ res/js/app.js
@@ -9,52 +9,56 @@ $(function() {
         event.preventDefault();
     });
 
- //    function quarter1() {
-	// 	$(".underline").css("margin-left", "0");
-	// 	alert("Changed!");
-	// };
-
-	// function quarter2() {
-	// 	$(".underline").css("margin-left", "25%");
-	// };
-
-	// function quarter3() {
-	// 	$(".underline").css("margin-left", "50%");
-	// };
-
-	// function quarter4() {
-	// 	$(".underline").css("margin-left", "75%");
-	// };
+    $(".navbar-nav").on("click", function() {
+    	$(".navbar-toggle").click();
+    })
 });
 
-var autocomplete;
+// global variables for map manipulation
+var autocompleteOrigin, autocompleteVia, map, directionsDisplay;
 
-
+// for changing the transport mode underbar
 function quarter1() {
 	$(".underline").css("margin-left", "0");
 };
 
 function quarter2() {
-	$(".underline").css("margin-left", "25%");
+	$(".underline").css("margin-left", "10%");
 };
 
 function quarter3() {
-	$(".underline").css("margin-left", "50%");
+	$(".underline").css("margin-left", "20%");
 };
 
 function quarter4() {
-	$(".underline").css("margin-left", "75%");
+	$(".underline").css("margin-left", "30%");
 };
 
+// initialize GMaps and its associating service classes
 function initMap() {
-	var mapElement = document.getElementById("gmaps");
-	var dest = new google.maps.LatLng(1.2936604, 103.857193);
-	var map = new google.maps.Map(mapElement, {
+	// map instantiation
+	var mapElement = document.getElementById("map");
+	var dest = {lat: 1.2936604, lng: 103.857193};
+	map = new google.maps.Map(mapElement, {
 		center: dest,
 		zoom: 15
 	});
 
-	var directionsService = new google.maps.DirectionsService();
+	// marker instantiation
+	var marker = new google.maps.Marker({
+		map: map,
+		position: dest,
+		title: "Suntec Convention & Exhibition Center",
+		visible: true
+	});
+	// console.log(marker);
+	var infoWindow = new google.maps.InfoWindow();
+	infoWindow.setContent("<div id='content'><p><strong>Suntec Convention & Exhibition Center</strong></p><p>1 Raffles Boulevard, <span class='street-address'>Suntec City</span>, <span class='country-name'>Singapore</span> <span class='postal-code'>039593</span></p></div>");
+	marker.addListener("click", function() {
+		infoWindow.open(map, marker);
+	});
+
+	// autocomplete initialization
 	var defaultBounds = new google.maps.LatLngBounds(
 		new google.maps.LatLng(1.146666, 103.598748),
 		new google.maps.LatLng(1.475193, 104.091667));
@@ -62,86 +66,184 @@ function initMap() {
 		bounds: defaultBounds
 	};
 	var origin = document.getElementById("origin");
-	autocomplete = new google.maps.places.Autocomplete(origin, options);
+	autocompleteOrigin = new google.maps.places.Autocomplete(origin, options);
+	var via = document.getElementById("via");
+	autocompleteVia = new google.maps.places.Autocomplete(via, options);
+
+	directionsDisplay = new google.maps.DirectionsRenderer(options);
+	directionsDisplay.setMap(map);
+
+
 };
 
+function getRoute() {
 
-function getUserData() {
 
-	// var orig = $("#origin").val();
-	// console.log(orig);
-	// var via = $("#via").val();
-	// console.log(via);
-	// var mode = $("input[name='mode']:checked").val();
-	// console.log(mode);
+	event.preventDefault();
 
-	var origin_location = autocomplete.getPlace().geometry.location;
-	var orig = new google.maps.LatLng(origin_location.lat(), origin_location.lng());
-
-	var request;
-
-	if (mode != "TRANSIT" && via != "") {
-		request = {
-			origin: orig,
-			destination: dest,
-			travelMode: mode,
-			waypoints: [{
-				location: via,
-				stopover: false
-			}],
-			region: "SG"
+	try {
+		// user input collection
+		var origin_location = autocompleteOrigin.getPlace().geometry.location;
+		// console.log(origin_location);
+		// console.log(autocompleteOrigin.getPlace().formatted_address);
+		var orig = new google.maps.LatLng(origin_location.lat(), origin_location.lng());
+		// console.log(orig.lat(), orig.lng());
+		var dest = new google.maps.LatLng(1.2936604, 103.857193);
+		// console.log(dest);
+		var directionsService = new google.maps.DirectionsService();
+		// var via = $("#via").val();
+		// console.log(autocompleteVia.getPlace());
+		var via;
+		if (autocompleteVia.getPlace() == null) {
+			via = "";
+		} else {
+			var via_location = autocompleteVia.getPlace().geometry.location;
+			via = new google.maps.LatLng(via_location.lat(), via_location.lng());
 		}
-	} else {
-		request = {
-			origin: orig,
-			destination: dest,
-			travelMode: mode,
-			region: "SG"
+		// console.log(via);
+		var mode = $("input[name='mode']:checked").val();
+		// console.log(mode);
+
+		// request compilation
+		var request;
+		if (mode != "TRANSIT" && via != "") {
+			request = {
+				origin: orig,
+				destination: dest,
+				travelMode: mode,
+				waypoints: [{
+					location: via,
+					stopover: false
+				}],
+				region: "SG"
+			}
+		} else {
+			request = {
+				origin: orig,
+				destination: dest,
+				travelMode: mode,
+				region: "SG"
+			}
 		}
+		// console.log(request);
+
+		// request submission and response handler
+		directionsService.route(request, function(result, status) {
+			// console.log(result);
+			// console.log(status);
+			switch (status) {
+				// if a response is available
+				case google.maps.DirectionsStatus.OK:
+					displayResult(result, mode);
+					directionsDisplay.setDirections(result);
+					break;
+				// if no response is available
+				case google.maps.DirectionsStatus.NOT_FOUND:
+					displayError("The location you entered cannot be found!");
+					break;
+				case google.maps.DirectionsStatus.ZERO_RESULTS:
+					displayError("No route found!");
+					break;
+				case google.maps.DirectionsStatus.INVALID_REQUEST:
+					displayError("Developer's apologize, it's my fault!");
+					break;
+				default:
+					displayError("Unknown error! Please re-submit the search");
+			}
+		});
 	}
-	console.log(request);
 
-	directionsService.route(request, function(result, status) {
-		console.log(result);
-		console.log(status);
-		switch (status) {
-			case google.maps.DirectionsStatus.OK:
-				displayResult(result);
-				break;
-			case google.maps.DirectionsStatus.NOT_FOUND:
-				displayError("The location you entered cannot be found!");
-				break;
-			case google.maps.DirectionsStatus.ZERO_RESULTS:
-				displayError("No route found!");
-				break;
-			case google.maps.DirectionsStatus.INVALID_REQUEST:
-				displayError("Fuck you Front-end Dev!");
-				break;
-			default:
-				displayError("Unknown error! Please re-submit the search");
-		}
-	});
+	// general exception handler
+	catch (e) {
+		displayError("Error! Please re-submit the search");
+	}
 };
 
-function displayResult(result) {
+// function for displaying result on the website
+function displayResult(result, mode) {
 	
+	// GMaps response handler
 	var route = result.routes[0];
-	console.log(route);
+	// console.log(route);
 	var warnings = route.warnings;
 	var total_distance = 0;
 	var total_duration = 0;
-	console.log("Length: " + route.legs.length);
+	// console.log("Length: " + route.legs.length);
+
+	// navigation response compiler
+	var details = "<li style='list-style: none'><i class='fa fa-circle-o'></i> ";
 	for (var l = 0; l < route.legs.length; l++) {
-		console.log(route.legs[l]);
-		total_distance += route.legs["0"].distance.value;
-		total_duration += route.legs["0"].duration.value;
+		var leg = route.legs[l];
+		// console.log(leg);
+
+		// general info computation
+		total_distance += leg.distance.value;
+		total_duration += leg.duration.value;
+		details += legBuilder(leg);
+
 	};
-	console.log(total_duration + "s");
-	console.log(total_distance + "m");
+	details += "</li><li style='list-style: none'><i class='fa fa-dot-circle-o'></i> Suntec Convention & Exhibition Center, Nicoll Hwy, Singapore</li>";
+	// console.log(details);
+	// console.log(total_duration + "s");
+	// console.log(total_distance + "m");
+	switch (mode) {
+		case "DRIVING":
+			mode = "Drive";
+			quarter1();
+			break;
+		case "TRANSIT":
+			mode = "Ride";
+			quarter2();
+			break;
+		case "BICYCLING":
+			mode = "Cycle";
+			quarter3();
+			break;
+		case "WALKING":
+			mode = "Walk";
+			quarter4();
+			break;
+	}
+	// show route cost in terms of time and distance
+	$("#route-cost").text(mode + " " + (total_distance/1000).toFixed(1) + " km, " + (total_duration/60).toFixed(1) + " min");
+	// show warnings from GMaps
+	if (route.warnings.length != 0) {
+		$("#route-warning").text(route.warnings[0]);
+	} else {
+		$("#route-warning").empty(); // or empty placeholder if there's none
+	}
+	// show route details (navigation)
+	$("#route-details").html(details);
+
 };
 
+// function for compiling details for certain legs in route
+function legBuilder(leg) {
+	var result = leg.start_address + "\n" + "<ul>";
+	var steps = leg.steps;
+	var style;
+	for (var s = 0; s < steps.length; s++) {
+		if (steps[s].instructions.toLowerCase().includes("left")) {
+			style = "<li style='list-style: none'><i class='fa fa-arrow-left list-marker'></i> "
+		} else if (steps[s].instructions.toLowerCase().includes("right")) {
+			style = "<li style='list-style: none'><i class='fa fa-arrow-right list-marker'></i> "
+		} else {
+			style = "<li style='list-style: none'><i class='fa fa-long-arrow-up list-marker'></i> ";
+		}
+		var instruction = style + steps[s].instructions + "</li>";
+		result += instruction;
+	}
+	result += "</ul>";
+	// console.log(result);
+	return result;
+}
+
+// function for displaying error message in warning placeholder
 function displayError(errorMessage) {
-	var errorWindow = "<div style='padding: 10%'><p class='text-warning'>" + errorMessage + "</p></div>";
+	// console.log(errorMessage)
+	$("#route-cost").empty();
+	$("#route-details").empty();
+	$("#route-warning").text(errorMessage);
 };
 
 // This example displays an address form, using the autocomplete feature 

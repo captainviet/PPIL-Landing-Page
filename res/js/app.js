e82f8c7ad4cf2953c3a7ad944205bd819dee6046
xdang <xdang@paypal.com>
2016-07-07 17:47:30 +0800

    Thu 7 Jul - Testing Upfront display

--- res/js/app.js
+++ res/js/app.js
@@ -53,7 +53,7 @@ $(function() {
 
     $(document).click(function(e) {
     	var id = e.target.id;
-    	var img = ["rohan-img", "iswaran-img"];
+    	var img = ["iswaran-img"];
     	for (var i = 0; i < img.length; i++) {
 	    	if (id != img[i]) {
 				$("#" + img[i]).animate({
@@ -70,6 +70,52 @@ $(function() {
 		    	});
 		    }
 		}
+    });
+
+    $("#iswaran-img").on("click", function () {
+    	console.log("clcok");
+        // close button
+        var closeBtn = $('<a href="#" data-rel="back" class="ui-btn-right ui-btn ui-btn-b ui-corner-all ui-btn-icon-notext ui-icon-delete ui-shadow">Close</a>');
+
+        // text you get from Ajax
+        var content = "<p>Mr. Iswaran Info</p>";
+
+        // Popup body - set width is optional - append button and Ajax msg
+        var popup = $("<div/>", {
+            "data-role": "popup"
+        }).css({
+            width: $(window).width() / 2.5 + "px",
+            padding: 5 + "px"
+        }).append(closeBtn).append(content);
+
+        console.log(popup);
+
+        // Append it to active page
+        $.mobile.pageContainer.append(popup);
+
+        // Create it and add listener to delete it once it's closed
+        // open it
+        $("[data-role=popup]").popup({
+            dismissible: false,
+            history: false,
+            theme: "b",
+            /* or a */
+            positionTo: "window",
+            overlayTheme: "b",
+            /* "b" is recommended for overlay */
+            transition: "pop",
+            beforeposition: function () {
+                $.mobile.pageContainer.pagecontainer("getActivePage")
+                    .addClass("blur-filter");
+            },
+            afterclose: function () {
+                $(this).remove();
+                $(".blur-filter").removeClass("blur-filter");
+            },
+            afteropen: function () {
+                /* do something */
+            }
+        }).popup("open");
     });
 
 });
